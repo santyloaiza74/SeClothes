2553637c82f0b9ebf51dbc3e271cec5e0aa0d8aa
santyloaiza74 <santyloaiza74@gmail.com>
2023-11-10 22:26:00 -0500

    Backend funcionando

--- Backend/src/database/models/index.js
+++ Backend/src/database/models/index.js
@@ -5,22 +5,26 @@ const { Venta, VentasSchema} = require('./venta.model')
 const { Producto, ProductoSchema } = require('./producto.model')
 const { Subasta, SubastaSchema } = require('./subasta.model')
 const { Puja, PujasSchema } = require('./pujas.model')
+const {Vendedor,VendedorSchema}=require('./vendedor.model')
 
 function setupModels(sequelize) {
     Login.init(LoginSchema, Login.config(sequelize))
-    Login.associate(sequelize.models)
     Comprador.init(CompradorSchema,Comprador.config(sequelize))
-    Comprador.associate(sequelize.models)
     Carrito.init(CarritoSchema, Carrito.config(sequelize))
-    Carrito.associate(sequelize.models)
     Venta.init(VentasSchema,Venta.config(sequelize))
-    Venta.associate(sequelize.models)
     Producto.init(ProductoSchema, Producto.config(sequelize))
-    Producto.associate(sequelize.models)
     Subasta.init(SubastaSchema, Subasta.config(sequelize))
-    Subasta.associate(sequelize.models)
     Puja.init(PujasSchema, Puja.config(sequelize))
+    Vendedor.init(VendedorSchema, Vendedor.config(sequelize))
+
+    Login.associate(sequelize.models) 
+    Comprador.associate(sequelize.models)
+    Carrito.associate(sequelize.models)
+    Venta.associate(sequelize.models)
+    Producto.associate(sequelize.models)
+    Subasta.associate(sequelize.models)
     Puja.associate(sequelize.models)
+    Vendedor.associate(sequelize.models)
 }
 
 module.exports = setupModels
